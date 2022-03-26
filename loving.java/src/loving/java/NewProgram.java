79e3a8c8fbbde1594a02c3c9d7816c83948d48dc
KauserShaikh7 <101319050+KauserShaikh7@users.noreply.github.com>
2022-03-26 12:51:37 +0530

    Update NewProgram.java

--- loving.java/src/loving/java/NewProgram.java
+++ loving.java/src/loving/java/NewProgram.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class NewProgram {
 
 	public static void main(String[] args) {
+		int a = 65; 
+		int b = 35;
+		System.out.println("The sum of a and b is =" + Integer.sum( a, b));
 		System.out.println("I love to Program");
 		System.out.println("I am learning Java");
 		
