975145c20b5ca9b5c57fd29ff5479e6f758c7322
hp <hp@192.168.0.12>
2022-03-26 11:32:45 +0530

    First Commit Uploading One File

---
+++ loving.java/src/loving/java/NewProgram.java
@@ -0,0 +1,14 @@
+package loving.java;
+
+import java.util.Scanner;
+
+public class NewProgram {
+
+	public static void main(String[] args) {
+		System.out.println("I love to Program");
+		System.out.println("I am learning Java");
+		
+	 }
+	}
+	
+
