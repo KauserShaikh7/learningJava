e93660cf94642542a0ee7588812ffeaa8ae4500c
hp <hp@192.168.0.12>
2022-03-26 11:18:09 +0530

    First Commit uploading one file

---
+++ loving.java/src/loving/java/learningYes.java
@@ -0,0 +1,15 @@
+package loving.java;
+
+public class learningYes {
+
+	public static void main(String[] args) {
+		int a = 65; 
+		int b = 35;
+		System.out.println("The sum of a and b is =" + Integer.sum( a, b));
+		System.out.println("kauser");
+		System.out.println("ggcghdh");     
+		System.out.println("int 1 + int 2");
+		}
+	}
+	
+	
